d273241cd9157f3acf9033437ea6f0f7196648b9
Ipan <irfanjulian23@gmail.com>
2022-11-24 23:50:25 +0700

    slicing page pekerja

--- hirejob/src/App.js
+++ hirejob/src/App.js
@@ -1,13 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import ProfilePekerja from "./page/profile/employee/mainProfile";
+import EditProfilePekerja from "./page/profile/employee/editProfile";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Routes>
-          <Route path="/">{/* <Route index element={<Home />} /> */}</Route>
+          {/* <Route path="/"><Route index element={<Home />} /></Route> */}
+          <Route path='/' element={<Navigate to="Home" replace="true"/>} />
+          <Route path="/profilePekerja" element={<ProfilePekerja/>} />
+          <Route path="/editProfile1" element={<EditProfilePekerja/>} />
         </Routes>
       </div>
     </div>
