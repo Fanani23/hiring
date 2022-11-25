4579a1ff17b5a001f3b6fb09802488905dcb9af6
Fanani23 <pramudiasf23@gmail.com>
2022-11-25 16:04:52 +0700

    landing page before login

--- hirejob/src/App.js
+++ hirejob/src/App.js
@@ -1,13 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Routes>
-          <Route path="/">{/* <Route index element={<Home />} /> */}</Route>
+          <Route path="/">
+            <Route index element={<LandingPage />} />
+          </Route>
         </Routes>
       </div>
     </div>
---
+++ hirejob/src/assets/index.js
@@ -0,0 +1,29 @@
+import HomeLogo from "./HomeLogo.svg";
+import HomeTr from "./Home-tr.svg";
+import HomeCl from "./Home-cl.svg";
+import HomeBr from "./Home-br.svg";
+import CheckCr from "./Check-cr.svg";
+import CheckBl from "./Check-bl.svg";
+import CarouselL from "./CarouselL.svg";
+import CarouselC from "./CarouselC.svg";
+import CarouselR from "./CarouselR.svg";
+import HomeBb from "./HomeBb.svg";
+import HomeLb from "./HomeLb.svg";
+import HomeLogoB from "./HomeLogoB.svg";
+
+const Assets = {
+  HomeLogo,
+  HomeTr,
+  HomeCl,
+  HomeBr,
+  CheckCr,
+  CheckBl,
+  CarouselL,
+  CarouselC,
+  CarouselR,
+  HomeBb,
+  HomeLb,
+  HomeLogoB,
+};
+
+export default Assets;
