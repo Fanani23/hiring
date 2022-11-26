7fbf4910d46aef771292e1e8d740345543560197
Lidiya135 <lidiyawati315@gmail.com>
2022-11-26 18:15:57 +0700

    up branch lidiya

--- hirejob/src/App.js
+++ hirejob/src/App.js
@@ -1,13 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import LoginEmployee from "../src/pages/Auth/Login/Employee";
+import LoginRecruiter from "../src/pages/Auth/Login/Recruiter";
+import RegisterEmployee from "../src/pages/Auth/Register/Employee";
+import RegisterRecruiter from "../src/pages/Auth/Register/Recruiter";
+import ResetPassword from "../src/pages/Auth/ResetPassword"
+import KonfirmasiResetPassword from "../src/pages/Auth/KonfirResetPassword";
+import RequestPassword from "../src/pages/Auth/requestPass";
+import Chat from "../src/pages/Chat"
+import Notifikasi from "./pages/Notifikasi";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Routes>
-          <Route path="/">{/* <Route index element={<Home />} /> */}</Route>
+          <Route path='/loginEmployee' element={<LoginEmployee />} />
+          <Route path='/loginRecruiter' element={<LoginRecruiter />} />
+          <Route path='/registerEmploye' element={<RegisterEmployee />} />
+          <Route path='/registerRecruiter' element={<RegisterRecruiter />} />
+          <Route path='/resetPassword' element={<ResetPassword />} />
+          <Route path='/konfirmasiResetPassword' element={<KonfirmasiResetPassword />} />
+          <Route path='/requestPassword' element={<RequestPassword />} />
+          <Route path='/Chat' element={<Chat />} />
+          <Route path='/Notifikasi' element={<Notifikasi />} />
         </Routes>
       </div>
     </div>
---
+++ hirejob/src/pages/Auth/Login/Recruiter.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './Login.module.css'
+import Peworld from '../../../img/Group 978.png';
+
+export default function LoginRecruiter() {
+
+  return (
+   <div class={styles.container}>
+        <div class={styles.bgImg}>
+            <img src={Peworld} class="pl-10 pt-8" alt=''/>
+            <h1 class="font-bold font-44px tracking-wider leading-10 text-white pt-32 px-12 text-4xl">
+                Temukan developer 
+                berbakat & terbaik 
+                di berbagai bidang
+                keahlian
+            </h1>
+        </div>
+            <div class="pl-24 py-12">
+                <h1 class="font-semibold text-3xl">Halo, PewPeople</h1>
+                <p class="py-2 mb-4" text-base>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer purus enim, dignissim nec mauris nec
+                </p>
+                <form className={styles.form}>
+                    <p class="text-sm text-slate-400">Email</p>
+                    <input type="email" className=" form-control mb-3 py-2 w-full " name="email"  placeholder="Masukan alamat email"/>
+                    <p class="text-sm text-slate-400">Kata Sandi</p>
+                    <input type="password" className="form-control mb-3 py-2 w-full" name="password" placeholder="Masukkan kata sandi"/>
+                    <p class="text-right text-sm my-3">
+                        <a class="text-yellow-500 text-slate-700 text-base" href="/ResetPassword">Lupa Kata Sandi?</a>
+                    </p>
+                    <button type='submit' class="text-center bg-yellow-500 text-white w-full py-2 my-2 rounded font-bold text-base ">Masuk</button>
+                </form>
+                <p class="text-center text-sm text-slate-700">
+                    Anda belum punya akun?
+                    <a class="text-yellow-500" href="/register"> Daftar disini</a>
+                </p>
+            </div>
+        </div>
+  )
+}
