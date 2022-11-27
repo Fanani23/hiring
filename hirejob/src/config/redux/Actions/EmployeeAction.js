e70a3806ac1277b593a79b397240669e00aa5084
Ipan <irfanjulian23@gmail.com>
2022-11-27 22:57:26 +0700

    setup redux

---
+++ hirejob/src/config/redux/Actions/EmployeeAction.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const EmployeeRegister = (data, navigate) => async(dispatch) => {
+    try {
+        dispatch({
+            type: 'REGISTER_EMPLOYEE_PENDING',    
+        })
+        const result = await axios.post(`http://localhost:8000/employee/register`, data)
+        const employee = result.data.data
+        dispatch({
+            type: 'REGISTER_EMPLOYEE_SUCCESS', payload: employee
+        })
+        console.log('employee register success');
+    } catch (error) {
+        console.log('employee register error');
+        console.log(error);
+    }    
+}
+
+export default EmployeeRegister
