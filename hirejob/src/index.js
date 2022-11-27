a84a3aa14097aa6da651f3f4726bbf706a7c4da6
Ipan <irfanjulian23@gmail.com>
2022-11-28 00:27:12 +0700

    fix

--- hirejob/src/index.js
+++ hirejob/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./config/redux/Store";
+import Store from "./config/Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
